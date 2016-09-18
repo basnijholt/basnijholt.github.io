31039a3bc595b0fccb20fddd56797ce77c8364a5
Bas Nijholt <basnijholt@gmail.com>
2016-09-18 17:55:26 +0200

    Add markdown filter

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,10 @@
-gem 'jekyll-scholar'
+require 'jekyll/scholar'
+require 'uri'
+
+module MarkdownFilter
+  class Markdown < BibTeX::Filter
+    def apply(value)
+      value.to_s.gsub(URI.regexp(['http','https','ftp'])) { |c| "[#{$&}](#{$&})" }
+    end
+  end
+end
