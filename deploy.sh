750f91e9dae3d21201bee33b90ca286cd9d3299b
Bas Nijholt <basnijholt@gmail.com>
2016-10-08 14:34:34 +0200

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ./compile.sh
+  bundle exec jekyll build -d out/
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -38,11 +38,6 @@ cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
